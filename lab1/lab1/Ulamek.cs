6b742d60ed19e32b13ef27ae8ab09b765fb650fc
milena.suchocka <sumilena@wp.pl>
2022-03-10 11:36:28 +0100

    Początek

---
+++ lab1/lab1/Ulamek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Ulamek: IEquatable<T>, IComparable<T>
+    {
+        private int licznik, mianownik;
+
+        public Ulamek(int Licznik, int Mianownik)
+        {
+            licznik = Licznik;
+            mianownik = Mianownik;
+        }
+        public Ulamek()
+        {
+            int licznik = 0;
+            int mianownik = 1;
+        }
+        public Ulamek(Ulamek ulamek)
+        {
+            licznik = ulamek.licznik;
+            mianownik = ulamek.mianownik;
+        }
+        public override string ToString()
+        {
+
+        }
+
+
+    }
+}
